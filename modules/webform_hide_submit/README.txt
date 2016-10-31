91e6e29e9bb3d001d8e3a0355cbba3039b25a7df
Jairo Escobar <support@jairoescobar.com>
2016-10-31 18:54:36 -0500

    Thm_Teraquant

---
+++ modules/webform_hide_submit/README.txt
@@ -0,0 +1,19 @@
+Description
+-----------
+
+Provides ability to hide submit or next button until all required fields
+are filled.
+
+By default there are three options to handle the behaviour for the webform
+action button. After installing the module, please visit the webform node edit
+page and choose the preferred option:
+* Display button — Default behaviour, button will be displayed as usual
+* Disable button — Button will be displayed but disabled until all required
+  fields are filled
+* Hide button — Button will be hidden until all required fields are filled.
+
+Dependencies
+------------
+
+* Webform 4
+* Field Group
---
+++ themes/teraquant/js/drupal.js
@@ -0,0 +1,45 @@
+(function() {
+  
+  var $ = jQuery;
+
+
+  Drupal.behaviors.href_click = {
+    attach: function (context, settings) {
+       $('a[href="#"]', context).click(function() {
+        return false;
+       });
+    }
+  };
+
+  Drupal.behaviors.cart_remove_wrap = {
+    attach: function (context, settings) {
+      $('.cart-remove-wrap a', context).click(function() {
+        $(this).parent().find('input').click();
+        return false;
+      });
+    }
+  };
+
+  Drupal.behaviors.products_filter = {
+    attach: function (context, settings) {
+      if($('#block-rhythm-cms-products-filter').length > 0 && $('#edit-commerce-price-amount-wrapper').length > 0) {
+        $('#edit-commerce-price-amount-wrapper').hide();
+        $('.products-filter-from input').val($('#edit-commerce-price-amount-wrapper #edit-commerce-price-amount-min').val());
+        $('.products-filter-to input').val($('#edit-commerce-price-amount-wrapper #edit-commerce-price-amount-max').val());
+        $('#block-rhythm-cms-products-filter button').click(function() {
+          $('#edit-commerce-price-amount-wrapper #edit-commerce-price-amount-min').val($('.products-filter-from input').val());
+          $('#edit-commerce-price-amount-wrapper #edit-commerce-price-amount-max').val($('.products-filter-to input').val());
+          $('#edit-commerce-price-amount-wrapper').closest('form').submit();
+          return false;
+        });
+      }
+    }
+  };
+
+  Drupal.behaviors.tb_megamenu_align = {
+    attach: function (context, settings) {
+      $('.mega-align-right .mn-sub', context).addClass('to-left');
+    }
+  };
+
+}());
